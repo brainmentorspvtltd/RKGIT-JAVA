2f269efe34f92cbca9f753908fc091ff0d0c871c
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-09-21 16:36:38 +0530

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,21 @@
+// Java is OOPS
+// javac - It is for Compile the Java Code
+// Coming from JDK
+// class - reserved keyword - Create a class (Blue Print)
+public class FirstClass{
+// class SecondClass{
+// void  nothing return to JRE
+// public - class scope , can be access outside the package (folder)
+// static - it is load the things when class is loaded in the memory.
+// String args[] - String is a collection of chars , String is a predefine class in java
+// args it just a variable name and it can be any thing
+// args it is an array , N Arguments
+// String args[] - For Command Line Arguments
+public static void main(String args){
+    System.out.println("Hello Java");
+    // print is a function which is used to print on screen
+    // out is a predefine object 
+    // System is a predefine class and out object reside in System class
+    }
+
+}
